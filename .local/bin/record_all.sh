07d23409ad9cf7f220b10e6c3050285bfc3e7a05
watari <watari@techwebunraveled.xyz>
2026-04-16 15:48:05 +0300

    added pause & resume

--- .local/bin/record_all.sh
+++ .local/bin/record_all.sh
@@ -7,22 +7,35 @@ if [ -z "$name" ]; then
 fi
 
 base="${name%.*}"
+outfile="${base}.mkv"
 
-# pre-buffer PulseAudio properly
-ffmpeg -f pulse -thread_queue_size 1024 -i default -t 1 -f null - 2>/dev/null
+ffmpeg -f pulse -thread_queue_size 4096 -i default -t 1 -f null - 2>/dev/null
+
+sleep 1
 
 nohup ffmpeg -hide_banner \
   -loglevel info \
   -stats \
+  -use_wallclock_as_timestamps 1 \
+  -vsync 1 \
+  -async 1 \
+  -thread_queue_size 4096 \
   -f x11grab \
+  -framerate 60 \
   -video_size "$(xdpyinfo | awk '/dimensions/{print $2}')" \
   -i "$DISPLAY" \
-  -f pulse -thread_queue_size 1024 -i default \
-  -c:v libx264 -preset veryfast -crf 23 \
+  -f pulse \
+  -i default \
+  -c:v libx264 \
+  -preset veryfast \
+  -crf 23 \
+  -pix_fmt yuv420p \
   -c:a libopus \
-  "${base}.mkv" \
+  -af "aresample=async=1:first_pts=0" \
+  "$outfile" \
   > /tmp/ffmpeg.log 2>&1 &
 
-pgrep -n ffmpeg > /tmp/ffmpeg.pid
+PID=$!
+echo "$PID" > /tmp/ffmpeg.pid
 
-notify-send "Recording started" "${base}.mkv"
+notify-send "Recording started" "$outfile"
